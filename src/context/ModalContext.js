c8c4fcbaa09e55cafd60e35ca8cf50d5d92e5724
lucasggonzalez94 <lucasggonzalez94@outlook.com>
2021-01-29 15:20:53 -0300

    Subida del proyecto

---
+++ src/context/ModalContext.js
@@ -0,0 +1,46 @@
+import React, {createContext, useEffect, useState} from 'react';
+import axios from 'axios'
+
+// Crear el context
+export const ModalContext = createContext()
+
+const ModalProvider = (props) => {
+
+    // State del provider
+    const [idreceta, guardarIdReceta] = useState(null)
+    const [informacion, guardarReceta] = useState({})
+
+    // Una vez que seleccionamos una receta, llamar la api
+    useEffect(() => {
+        
+        const obtenerReceta = async () => {
+
+            if (!idreceta) return
+
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`
+
+            const resultado = await axios(url)
+
+            // console.log(resultado.data.drinks[0])
+
+            guardarReceta(resultado.data.drinks[0])
+
+        }
+        obtenerReceta()
+
+    }, [idreceta])
+
+    return (
+        <ModalContext.Provider
+            value={{
+                informacion,
+                guardarIdReceta,
+                guardarReceta
+            }}
+        >
+            {props.children}
+        </ModalContext.Provider>
+    )
+}
+
+export default ModalProvider
